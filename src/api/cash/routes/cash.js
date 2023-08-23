59e544d0292b8b945ea41e0c3056e70d9711b257
Cristian T. Torres <cristiantomastorres@gmail.com>
2023-08-23 17:10:26 -0300

    asdf

--- src/api/cash/routes/cash.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cash router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::cash.cash');
--- src/api/cash/services/cash.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cash service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::cash.cash');

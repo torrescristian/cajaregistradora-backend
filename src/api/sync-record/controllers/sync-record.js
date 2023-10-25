4e21ec81bb34357d67ba8cceae5bb9bc7263f610
Cristian T. Torres <cristiantomastorres@gmail.com>
2023-10-25 15:45:48 -0300

    fix: sync-records

---
+++ src/api/sync-record/controllers/sync-record.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sync-record controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sync-record.sync-record');

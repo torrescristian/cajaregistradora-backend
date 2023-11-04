3399fcdcbae44b113186345e3655ca3f4e633f9a
Cristian Torres <cristiantomastorres@gmail.com>
2023-11-03 21:31:41 -0300

    Dev (#4)
    * add product type
    
    * add product type controller

--- src/api/product-type/controllers/product-type.js
+++ src/api/product-type/controllers/product-type.js
@@ -5,19 +5,60 @@
  * product-type controller
  */
 
-const {
-  createControllerKey,
-  findPageInStore,
-  findOneInStore,
-} = require('../../../../libs/utils');
-
-const MODEL_KEY = 'product-type';
-
-const CONTROLLER_KEY = createControllerKey(MODEL_KEY);
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
+const {
+  findPageInStore,
+  findOneInStore,
+  createControllerKey,
+} = require('../../../../libs/utils');
+
+const CONTROLLER_KEY = createControllerKey('product-type');
+
 module.exports = createCoreController(CONTROLLER_KEY, ({ strapi }) => ({
+  async find(ctx) {
+    const productsTypes = await findPageInStore({
+      strapi,
+      key: CONTROLLER_KEY,
+      ctx,
+      orderBy: {
+        name: 'asc',
+      },
+    });
+
+    return ctx.send(productsTypes);
+  },
+
+  async findOne(ctx) {
+    const productType = await findOneInStore({
+      strapi,
+      key: CONTROLLER_KEY,
+      ctx,
+    });
+
+    if (!productType) {
+      return ctx.throw(404, 'Product type not found');
+    }
+
+    return ctx.send(productType);
+  },
+
+  async update(ctx) {
+    const productType = await findOneInStore({
+      strapi,
+      key: CONTROLLER_KEY,
+      ctx,
+      populate: [],
+    });
+
+    if (!productType) {
+      return ctx.throw(404, 'Product not found');
+    }
+
+    const updatedProduct = await super.update(ctx);
+    ctx.send(updatedProduct);
+  },
+
   async create(ctx) {
     const userId = ctx.state.user.id;
 
@@ -37,41 +78,14 @@ module.exports = createCoreController(CONTROLLER_KEY, ({ strapi }) => ({
     });
 
     if (!store) {
-      return ctx.throw(404, 'Cash balance not found');
+      return ctx.throw(404, 'Store not found');
     }
 
     ctx.request.body.data.store = store.id;
-    const createdOrder = await super.create(ctx);
 
-    ctx.send(createdOrder);
-  },
-  async find(ctx) {
-    const cashBalances = await findPageInStore({
-      strapi,
-      key: CONTROLLER_KEY,
-      ctx,
-      orderBy: {
-        id: 'desc',
-      },
-    });
+    // create productType
+    const createdProductType = await super.create(ctx);
 
-    if (!cashBalances) {
-      return ctx.throw(404, 'Cash balance not found');
-    }
-
-    ctx.send(cashBalances);
-  },
-  async findOne(ctx) {
-    const cashBalances = await findOneInStore({
-      strapi,
-      key: CONTROLLER_KEY,
-      ctx,
-    });
-
-    if (!cashBalances) {
-      return ctx.throw(404, 'Cash balance not found');
-    }
-
-    ctx.send(cashBalances);
+    ctx.send(createdProductType);
   },
 }));

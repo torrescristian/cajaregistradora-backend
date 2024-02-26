1335db75b0b7e14617f9c42feb1fef27bce1ed8d
Cristian T. Torres <cristiantomastorres@gmail.com>
2024-02-26 12:07:00 -0300

    feat: deposit controller

--- src/api/deposit/controllers/deposit.js
+++ src/api/deposit/controllers/deposit.js
@@ -1,9 +1,78 @@
+// @ts-check
 'use strict';
 
+const {
+  createControllerKey,
+  findPageInStore,
+  findOneInStore,
+} = require('../../../../libs/utils');
+
 /**
- * deposit controller
- */
+ * expense controller
+*/
+
+const MODEL_KEY = 'deposit';
+
+const CONTROLLER_KEY = createControllerKey(MODEL_KEY);
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::deposit.deposit');
+// @ts-ignore
+module.exports = createCoreController(CONTROLLER_KEY, ({ strapi }) => ({
+  async create(ctx) {
+    const userId = ctx.state.user.id;
+
+    const store = await strapi.db.query(createControllerKey('store')).findOne({
+      where: {
+        $or: [
+          {
+            owner: {
+              id: userId,
+            },
+          },
+          {
+            employees: [userId],
+          },
+        ],
+      },
+    });
+
+    if (!store) {
+      return ctx.throw(404, 'Store not found');
+    }
+
+    ctx.request.body.data.store = store.id;
+    const created = await super.create(ctx);
+
+    ctx.send(created);
+  },
+  async find(ctx) {
+    const res = await findPageInStore({
+      strapi,
+      key: CONTROLLER_KEY,
+      ctx,
+      orderBy: {
+        id: 'desc',
+      },
+    });
+
+    if (!res) {
+      return ctx.throw(404, 'Deposit not found');
+    }
+
+    ctx.send(res);
+  },
+  async findOne(ctx) {
+    const res = await findOneInStore({
+      strapi,
+      key: CONTROLLER_KEY,
+      ctx,
+    });
+
+    if (!res) {
+      return ctx.throw(404, 'Deposit not found');
+    }
+
+    ctx.send(res);
+  },
+}));

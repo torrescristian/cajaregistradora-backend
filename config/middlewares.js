429c8708f2dc903380faf9cc0d8aea862fcfc846
Cristian T. Torres <cristiantomastorres@gmail.com>
2024-03-26 17:17:59 -0300

    feat: update whitelist

--- config/middlewares.js
+++ config/middlewares.js
@@ -19,7 +19,7 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:8888', 'http://localhost:3000', 'http://localhost:1337', 'https://control.cajaregistradora.app', 'https://cajaregistradora.app', 'https://api.cajaregistradora.app']
+      origin: ['http://localhost:8888', 'http://localhost:3000', 'http://localhost:1337', 'https://api-pos.up.railway.app', 'https://api.cajaregistradora.app']
     }
   },
   // 'strapi::cors',

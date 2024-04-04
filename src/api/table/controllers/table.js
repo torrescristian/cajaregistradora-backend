4b1803078c06c3b0ddfc599c04c88ad5ac1424d3
Cristian T. Torres <cristiantomastorres@gmail.com>
2024-04-03 22:41:06 -0300

    feat: table, table-category & delivery controllers

--- src/api/table/controllers/table.js
+++ src/api/table/controllers/table.js
@@ -1,9 +1,78 @@
+// @ts-check
 'use strict';
+
+const {
+  createControllerKey,
+  findPageInStore,
+  findOneInStore,
+} = require('../../../../libs/utils');
 
 /**
  * table controller
  */
 
+const MODEL_KEY = 'table';
+
+const CONTROLLER_KEY = createControllerKey(MODEL_KEY);
+
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::table.table');
+// @ts-ignore
+module.exports = createCoreController(CONTROLLER_KEY, ({ strapi }) => ({
+  async create(ctx) {
+    const userId = ctx.state.user.id;
+
+    const results = await strapi.db.query(createControllerKey('store')).findOne({
+      where: {
+        $or: [
+          {
+            owner: {
+              id: userId,
+            },
+          },
+          {
+            employees: [userId],
+          },
+        ],
+      },
+    });
+
+    if (!results) {
+      return ctx.throw(404, 'Store not found');
+    }
+
+    ctx.request.body.data.store = results.id;
+    const createdOrder = await super.create(ctx);
+
+    ctx.send(createdOrder);
+  },
+  async find(ctx) {
+    const results = await findPageInStore({
+      strapi,
+      key: CONTROLLER_KEY,
+      ctx,
+      orderBy: {
+        id: 'desc',
+      },
+    });
+
+    if (!results) {
+      return ctx.throw(404, 'Table not found');
+    }
+
+    ctx.send(results);
+  },
+  async findOne(ctx) {
+    const results = await findOneInStore({
+      strapi,
+      key: CONTROLLER_KEY,
+      ctx,
+    });
+
+    if (!results) {
+      return ctx.throw(404, 'Table not found');
+    }
+
+    ctx.send(results);
+  },
+}));
